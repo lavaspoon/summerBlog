3c7e0a3e23c779d1c96ca44437001dde88d3194d
spoon lava <lavaspoon@naver.com>
2022-01-27 23:58:38 +0900

    summerBlog 프로젝트 생성(스프링 고급편)

---
+++ summerBlog/src/main/java/hello/summerBlog/SummerBlogApplication.java
@@ -0,0 +1,13 @@
+package hello.summerBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummerBlogApplication.class, args);
+	}
+
+}
---
+++ summerBlog/src/test/java/hello/summerBlog/SummerBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.summerBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummerBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
