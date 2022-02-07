230726e04ed6451ee7d44461a9f4147514936ce7
lavaspoon <lavaspoon@naver.com>
2022-02-07 13:00:51 +0900

    TraceStatus 클래스 개발완료

--- summerBlog/src/main/java/hello/summerBlog/trace/TraceStatus.java
+++ summerBlog/src/main/java/hello/summerBlog/trace/TraceStatus.java
@@ -2,5 +2,25 @@ package hello.summerBlog.trace;
 
 //로그의 상태정보를 나타내는 클래스
 public class TraceStatus {
+    private TraceId traceId;
+    private Long StartTimeMs;
+    private String Message;
 
+    public TraceStatus(TraceId traceId, Long startTimeMs, String message) {
+        this.traceId = traceId;
+        StartTimeMs = startTimeMs;
+        Message = message;
+    }
+
+    public TraceId getTraceId() {
+        return traceId;
+    }
+
+    public Long getStartTimeMs() {
+        return StartTimeMs;
+    }
+
+    public String getMessage() {
+        return Message;
+    }
 }
