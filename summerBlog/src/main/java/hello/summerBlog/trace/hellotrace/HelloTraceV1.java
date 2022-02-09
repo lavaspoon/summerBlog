65bdda3a20efb9c6306fc48cb684360e279afefe
lavaspoon <lavaspoon@naver.com>
2022-02-09 12:58:04 +0900

    HelloTraceV1 로그 추적기 테스트해볼 클래스 작업완료

--- summerBlog/src/main/java/hello/summerBlog/trace/hellotrace/HelloTraceV1.java
+++ summerBlog/src/main/java/hello/summerBlog/trace/hellotrace/HelloTraceV1.java
@@ -1,6 +1,56 @@
 package hello.summerBlog.trace.hellotrace;
 
+import hello.summerBlog.trace.TraceId;
+import hello.summerBlog.trace.TraceStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
 //로그추적기를 테스트해볼 클래스
+@Slf4j
+@Component //싱글톤으로 스프링빈 등록해서 사용할 목적
 public class HelloTraceV1 {
 
+    private static final String START_PREFIX = "-->";
+    private static final String COMPLETE_PREFIX = "<--";
+    private static final String EX_PREFIX = "<X-";
+
+    public TraceStatus begin(String message) {
+        TraceId traceId = new TraceId();
+        Long startTimeMs = System.currentTimeMillis();
+        log.info("[{}] {}{}", traceId.getId(), addSpace(START_PREFIX,
+                traceId.getLevel()), message);
+        return new TraceStatus(traceId, startTimeMs, message);
+    }
+
+    public void end(TraceStatus status) {
+        complete(status, null);
+    }
+
+    public void exception(TraceStatus status, Exception e) {
+        complete(status, e);
+    }
+
+    private void complete(TraceStatus status, Exception e) {
+        Long stopTimeMs = System.currentTimeMillis();
+        long resultTimeMs = stopTimeMs - status.getStartTimeMs();
+        TraceId traceId = status.getTraceId();
+        if (e == null) {
+            log.info("[{}] {}{} time={}ms", traceId.getId(),
+                    addSpace(COMPLETE_PREFIX, traceId.getLevel()), status.getMessage(),
+                    resultTimeMs);
+        } else {
+            log.info("[{}] {}{} time={}ms ex={}", traceId.getId(),
+                    addSpace(EX_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs,
+                    e.toString());
+        }
+    }
+
+    private static String addSpace(String prefix, int level) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < level; i++) {
+            sb.append((i == level - 1) ? "|" + prefix : "|   ");
+        }
+        return sb.toString();
+    }
 }
+
