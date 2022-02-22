6bb5c3a07a5fb258fbbc51a76b3b03c4bb3509f3
spoon lava <lavaspoon@naver.com>
2022-02-22 11:03:00 +0900

    로그 추적기 V1 - 프로토타입 개발(HelloTraceV1Test 클래스 프로토타입 작성)

--- summerBlog/src/test/java/hello/summerBlog/trace/hellotrace/HelloTraceV1Test.java
+++ summerBlog/src/test/java/hello/summerBlog/trace/hellotrace/HelloTraceV1Test.java
@@ -1,7 +1,21 @@
 package hello.summerBlog.trace.hellotrace;
 
+import hello.summerBlog.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HelloTraceV1Test {
-
+    @Test
+    void begin_end(){
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.end(status);
+    }
+    @Test
+    void begin_exception(){
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.exception(status, new IllegalStateException());
+    }
 }
